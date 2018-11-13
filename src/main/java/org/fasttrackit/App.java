a91034b84180e8acb67e79b481371143d2989c71
Flaviu Ratiu <ratiu.flaviu@gmail.com>
2018-11-13 19:21:53 +0200

    a functional version of the game

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -1,52 +1,9 @@
 package org.fasttrackit;
 
-import java.time.LocalDateTime;
-
 public class App {
 
-    public static void main(String[] args) {
-        Rescuer rescuer = new Rescuer();
-        rescuer.setName("John");
-        rescuer.setMoney(300);
-
-        Animal animal = new Animal();
-        animal.setName("Chaika");
-        animal.setAge(4.5);
-        animal.setHealthLevel(10);
-        animal.setHappinessLevel(10);
-        animal.setFavoriteActivityName("Running");
-        animal.setFavoriteFoodName("Pizza");
-
-        EntertainmentActivity activity = new EntertainmentActivity("Running");
-
-        Food food = new Food("Purina");
-        food.setQuantity(10);
-        food.setPrice(79.99);
-        food.setInStock(true);
-        food.setExpiryDate(LocalDateTime.of(2018, 12, 31, 12, 0, 0));
-
-        MedicalStaff medic = new MedicalStaff();
-        medic.setName("Dr. Who");
-        medic.setSpecialization("General Medicine");
-
+    public static void main(String[] args) throws Exception {
         Game game = new Game();
-        game.setRescuer(rescuer);
-        game.setAnimal(animal);
-        game.setMedic(medic);
-
         game.start();
-
-        rescuer.feed(animal, food);
-        rescuer.entertain(animal, activity);
-
-        animal.expressJoy();
-
-        Animal dog = new Dog();
-        dog.setName("Panda Maci");
-        dog.expressJoy();
-
-        Animal fish = new Fish();
-        fish.setName("Bregović");
-        fish.expressJoy();
     }
 }
--- src/main/java/org/fasttrackit/Game.java
+++ src/main/java/org/fasttrackit/Game.java
@@ -2,8 +2,13 @@ package org.fasttrackit;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Game {
+
+    private static final int MAX_LEVEL = 10;
+    private static final int MIN_LEVEL = 7;
+    private static final int MAX_ITERATIONS = 20;
 
     private Rescuer rescuer;
     private Animal animal;
@@ -25,12 +30,40 @@ public class Game {
         availableActivities[1] = activity2;
     }
 
-    public void start() {
-        initFood();
+    public void start() throws Exception {
+        initAnimal();
         initActivities();
+        initFood();
+        initRescuer();
 
-        displayAvailableFood();
-        displayAvailableActivities();
+        nameAnimal();
+
+        for (int i = 1; i <= MAX_ITERATIONS; i++) {
+            requireActivity();
+            requireFeeding();
+
+            if (i % 3 == 0) {
+                animal.setHungerLevel(animal.getHungerLevel() + 1);
+                animal.setHappinessLevel(animal.getHappinessLevel() - 1);
+                System.out.println("***********");
+                System.out.println(animal.getName() + " is getting hungry and bored.");
+                System.out.println("Hunger level: " + animal.getHungerLevel());
+                System.out.println("Happiness level: " + animal.getHappinessLevel());
+                System.out.println("***********");
+            }
+
+            if (i == MAX_ITERATIONS) {
+                if (MIN_LEVEL < animal.getHungerLevel()) {
+                    throw new RuntimeException("Game over. " + animal.getName() + " is too poorly fed.");
+                }
+
+                if (MIN_LEVEL < animal.getHappinessLevel()) {
+                    throw new RuntimeException("Game over. " + animal.getName() + " is too poorly entertained.");
+                }
+
+                System.out.println("Congratulations! " + animal.getName() + " has been fed and entertained well enough.");
+            }
+        }
     }
 
     private void displayAvailableFood() {
@@ -51,27 +84,118 @@ public class Game {
         }
     }
 
-    public Rescuer getRescuer() {
-        return rescuer;
+    private void initAnimal() {
+        animal = new Dog();
+        animal.setAge(0.2);
+        animal.setFavoriteActivityName("Running");
+        animal.setFavoriteFoodName("Purina");
+        animal.setHungerLevel(MAX_LEVEL - 1);
+        animal.setHappinessLevel(1);
     }
 
-    public void setRescuer(Rescuer rescuer) {
-        this.rescuer = rescuer;
+    private void initRescuer() {
+        rescuer = new Rescuer();
+        rescuer.setMoney(100);
+        rescuer.setName(readRescuerName());
     }
 
-    public Animal getAnimal() {
-        return animal;
+    private String readRescuerName() {
+        System.out.println("Please type your name and press Enter.");
+
+        Scanner scanner = new Scanner(System.in);
+        String rescuerName = scanner.nextLine();
+
+        System.out.println("Welcome,  " + rescuerName);
+        System.out.println("***********");
+
+        return rescuerName;
     }
 
-    public void setAnimal(Animal animal) {
-        this.animal = animal;
+    private String readActivityName() {
+        System.out.println("Please select activity...");
+
+        Scanner scanner = new Scanner(System.in);
+        String activityName = scanner.nextLine();
+
+        System.out.println("Selected:  " + activityName);
+
+        List<String> availableActivityNames = new ArrayList<>();
+
+        for (EntertainmentActivity activity : availableActivities) {
+            if (activity != null) {
+                availableActivityNames.add(activity.getName());
+            }
+        }
+
+        if (!availableActivityNames.contains(activityName)) {
+            System.out.println(activityName + " is not available as an activity in this game.");
+            return readActivityName();
+        }
+
+        System.out.println("***********");
+
+        return activityName;
     }
 
-    public MedicalStaff getMedic() {
-        return medic;
+    private void requireActivity() throws Exception {
+        System.out.println(animal.getName() + "'s happiness level is: " + animal.getHappinessLevel() + ". Please select entertainment activity.");
+
+        displayAvailableActivities();
+
+        String activityName = readActivityName();
+
+        EntertainmentActivity activity = new EntertainmentActivity(activityName);
+
+        rescuer.entertain(animal, activity);
+
+        System.out.println("***********");
     }
 
-    public void setMedic(MedicalStaff medic) {
-        this.medic = medic;
+    private String readFoodName() {
+        System.out.println("Please select food...");
+
+        Scanner scanner = new Scanner(System.in);
+        String foodName = scanner.nextLine();
+
+        System.out.println("Selected:  " + foodName);
+
+        List<String> availableFoodNames = new ArrayList<>();
+
+        for (Food food : availableFood) {
+            availableFoodNames.add(food.getName());
+        }
+
+        if (!availableFoodNames.contains(foodName)) {
+            System.out.println(foodName + " is not available as a food type in this game.");
+            return readFoodName();
+        }
+
+        System.out.println("***********");
+
+        return foodName;
     }
+
+    private void requireFeeding() throws Exception {
+        System.out.println(animal.getName() + "'s hunger level is: " + animal.getHungerLevel() + ". Please select food.");
+
+        displayAvailableFood();
+
+        String foodName = readFoodName();
+        Food food = new Food(foodName);
+
+        rescuer.feed(animal, food);
+
+        System.out.println("***********");
+    }
+
+    private void nameAnimal() {
+        System.out.println("Please give a name for your rescued pet...");
+
+        Scanner scanner = new Scanner(System.in);
+        String animalName = scanner.nextLine();
+        animal.setName(animalName);
+        System.out.println(animalName + " says thanks.");
+        System.out.println("***********");
+    }
+
 }
